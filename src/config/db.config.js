57b5ec6a1f4d9e74742b3587d40e60b678eace0c
ardyanfalah <ardyanfalah@gmail.com>
2021-03-21 13:29:43 +0700

    tambah setting db

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,6 +1,13 @@
+// module.exports = {
+//     HOST: "localhost",
+//     USER: "root",
+//     PASSWORD: "",
+//     DB: "simet_appi_db"
+//   };
+
 module.exports = {
     HOST: "localhost",
-    USER: "root",
-    PASSWORD: "",
-    DB: "simet_appi_db"
+    USER: "privatea_privatea",
+    PASSWORD: "Alfalah18",
+    DB: "privatea_simet-appi"
   };
